6688852e7dd88117b91b1a72784bbbcab7fcc7a5
alvinsss <6449694@qq.com>
2019-01-06 11:19:24 +0800

    qa demo juint ant
    alvin qatest

---
+++ src/com/alvin/Calc.java
@@ -0,0 +1,18 @@
+package com.alvin;
+
+public class Calc {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+
+	}
+
+	// 加法
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	public int div(int i, int j) {
+		return i / j;
+	}
+
+}
